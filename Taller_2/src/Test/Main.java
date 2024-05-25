1bcf3bfb8a19b4005f526c3c13b6f289748cdf23
Juan_Arroyo <juanarroyobaquerizo2812@outlook.com>
2024-05-25 13:20:03 -0500

    .gitignore

--- Taller_2/src/Test/Main.java
+++ Taller_2/src/Test/Main.java
@@ -2,12 +2,43 @@ package Test;
 
 import java.util.Scanner;
 
+import serieNumerica.*;
+
 
 public class Main {
 
     static Scanner keyboard = new Scanner(System.in);
 
     public static void main(String[] args) {
+        ArroyoJuan ajSn = new ArroyoJuan();
+        // System.out.print("Ingrese el numero: ");
+        // int size = Integer.parseInt(keyboard.nextLine());
 
+        ajSn.jaS1(0);
+        System.out.println();
+        ajSn.jaS1(1);
+        System.out.println();
+        ajSn.jaS1(2);
+        System.out.println();
+        ajSn.jaS1(3);
+        System.out.println();
+        ajSn.jaS1(4);
+        System.out.println();
+        ajSn.jaS1(5);
+        System.out.println();
+        ajSn.jaS1(6);
+        System.out.println();
+        ajSn.jaS1(7);
+        System.out.println();
+        ajSn.jaS1(8);
+        System.out.println();
+        ajSn.jaS1(9);
+        System.out.println();
+        ajSn.jaS1(10);
+
+        // Creacion de propios metodos y variables
+        // ajSn.jaS1(size);
+        // System.out.println();
+        // ajSn.jaS7(size);
     }
 }
