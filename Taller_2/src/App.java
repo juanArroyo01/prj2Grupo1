7e01ff46cc108451bc94c310a09bbcf36d1ed8b9
Juan_Arroyo <juanarroyobaquerizo2812@outlook.com>
2024-05-24 21:08:30 -0500

    Test sout

--- Taller_2/src/App.java
+++ Taller_2/src/App.java
@@ -3,6 +3,6 @@ public class App {
     public static void main(String[] args) throws Exception {
     
         System.out.println("\t---> Taller 2 <---");
-    
+        System.out.println("Test 1");
     }
 }
