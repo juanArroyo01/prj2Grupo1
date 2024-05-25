3b3cc7ddeb0d63c446651b3a1950a0c5dd62f55e
Juan_Arroyo <juanarroyobaquerizo2812@outlook.com>
2024-05-25 01:06:05 -0500

    Method created in serieNumerica

--- Taller_2/src/serieNumerica/ArroyoJuan.java
+++ Taller_2/src/serieNumerica/ArroyoJuan.java
@@ -1,8 +1,20 @@
 package serieNumerica;
 
 public class ArroyoJuan {
-    
-    public void jaS1(int size){
 
+    /**
+     * Serie de Fibbonacci, la sumatoria de los dos ultimos
+     * numeros genera el siguiente numero.
+     * El usuario ingresa el tamanyo, size, y se genera una
+     * serie con el maximo de repeticiones que el usuario
+     * desee.
+     * 
+     * @param size
+     * @return result
+     */
+    public int jaS1(int size) {
+        int result = 0;
+        
+        return result;
     }
 }
