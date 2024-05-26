0bb568e05953adb5e38714e6f0276ec344b81c4e
Juan_Arroyo <juanarroyobaquerizo2812@outlook.com>
2024-05-25 22:12:57 -0500

    Juan's part done

--- Taller_2/src/Test/Main.java
+++ Taller_2/src/Test/Main.java
@@ -2,33 +2,12 @@ package Test;
 
 import java.util.Scanner;
 
-import figuras.ArroyoJuan;
-
 public class Main {
 
     static Scanner keyboard = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        ArroyoJuan aj = new ArroyoJuan();
-        // aj.jaF4(size, symb);
-            
-        aj.jaF7(0);
-        System.out.println();
-        aj.jaF7(1);
-        System.out.println();
-        aj.jaF7(2);
-        System.out.println();
-        aj.jaF7(3);
-        System.out.println();
-        aj.jaF7(4);
-        System.out.println();
-        aj.jaF7(5);
-        System.out.println();
-        aj.jaF7(6);
-        System.out.println();
-        aj.jaF7(7);
 
-        System.out.println();
     }
 }
--- Taller_2/src/figuras/ArroyoJuan.java
+++ Taller_2/src/figuras/ArroyoJuan.java
@@ -23,6 +23,13 @@ public class ArroyoJuan {
         }
     }
 
+    /**
+     * Figura escalonar con caracteres.
+     * El usuario ingresa tamanyo, se genera una
+     * figura a partir de los parametros pasados.
+     * 
+     * @param size
+     */
     public void jaF7(int size) {
         int aux = 4;
         for (int i = 0; i < size; i++) {
@@ -32,6 +39,27 @@ public class ArroyoJuan {
             }
             aux += 4;
             System.out.print('|');
+        }
+    }
+
+
+    /**
+     * Triangulo de Pascal.
+     * El usuario ingresa el size del triangulo, 
+     * se genera el triangulo a partir de los 
+     * parametros pasados.
+     * 
+     * @param size
+     */
+    public void jaf15(int size) {
+        int aux;
+        for (int i = 0; i < size; i++) {
+            aux = 1;
+            for (int j = 0; j <= i; j++) {
+                System.out.print(aux + "  ");
+                aux = aux * (i - j) / (j + 1);
+            }
+            System.out.println();
         }
     }
 
