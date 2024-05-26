e089ee2cc331356913cb7d4b45e8a304f479383b
Juan_Arroyo <juanarroyobaquerizo2812@outlook.com>
2024-05-25 19:05:09 -0500

    Solved

--- Taller_2/src/App.java
+++ Taller_2/src/App.java
@@ -3,6 +3,6 @@ public class App {
     public static void main(String[] args) throws Exception {
     
         System.out.println("\t---> Taller 2 <---");
-        System.out.println("Test 1: Fork Solucionado 2.0.1");
+        System.out.println("Test 1: Fork Solucionado 2.0.1 segundo branch");
     }
 }
